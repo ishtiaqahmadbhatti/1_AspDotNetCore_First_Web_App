e7e8f13eba9708b1de6160d8dd47cea6941097ff
Ishtiaq Ahmad <ishtiaq.uaar55@gmail.com>
2023-05-21 12:43:59 +0500

    Add project files.

---
+++ 1_AspDotNetCore_First_Web_App/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using _1_AspDotNetCore_First_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _1_AspDotNetCore_First_Web_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index page accessed");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
